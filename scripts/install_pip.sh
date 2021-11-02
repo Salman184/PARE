7168ddf453e3052f8cf38a9bebc86fac5c4dc54c
Salman184 <66486975+Salman184@users.noreply.github.com>
2021-11-02 21:03:31 +0000

    Update install_pip.sh

--- scripts/install_pip.sh
+++ scripts/install_pip.sh
@@ -2,10 +2,11 @@
 set -e
 
 echo "Creating virtual environment"
-python3.7 -m venv pare-env
+pip3 install virtualenv
+virtualenv pare-env
+
 echo "Activating virtual environment"
 
-source $PWD/pare-env/bin/activate
+source $PWD/pare-env/bin/activate; pip3 install libturbojpeg
 
-sudo apt-get install libturbojpeg
 $PWD/pare-env/bin/pip install -r requirements.txt
